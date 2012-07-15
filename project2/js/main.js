0aef5f506fb7e42b1e5eaac697894df44fccb87f
Matthew Richter <mrichter@fullsail.edu>
2012-07-15 19:08:09 -0400

    Updated $("#getCSV") data is being pulled in from CSV file
    made some minor semantic changes to xml, csv & json data

--- project2/js/main.js
+++ project2/js/main.js
@@ -82,11 +82,10 @@ $("#home").on('pageinit',function(){
 			}
 		});
 });
-
 // get XML
 	$("#getXML").on("click", function(){
-		$("#dataHolder").empty();
 		console.log("#getXML");
+		$("#dataHolder").empty();
 		$.ajax({
 			url: 		"data/data.xml",
 			type:		"GET",
@@ -126,46 +125,57 @@ $("#home").on('pageinit',function(){
 	}
 });
 });
-});
 // get CSV
-/*
 	$("#getCSV").on("click", function(){
+		console.log("#getCSV");
 		$("#dataHolder").empty();
 		$.ajax({
 			url: 		"data/data.csv",
 			type:		"GET",
 			dataType:	"text",
-			success:	function(data){
-			var textLines = data.split(/\r\n|\n/);
-			var headers = textLines[0].split(",");
-			var lines = [];
-			
-			for (var i=1, j=textLines.length;i++)
-				var data = textLines[i].split(",");
-				if (data.length == headers.length) 
+			success:	function(text){
+			console.log(text);
+// \r\n  return cariage then new line then new line
+				var textLines = text.split(/\r\n|\n/);
+	// var headers is the value headers - project, prohect type , first name etc...
+				var headers = textLines[0].split(",");
+				var lines = [];	
+				for (var i=1; i<textLines.length;i++){
+					var text = textLines[i].split(",");
+					if (text.length == headers.length){
+						var projectData = [];
+						
+						for (var j=0; j<headers.length;j++){
+							projectData.push(text[j]);
+						}
+						lines.push(projectData);
+					} 
+				}
+				for (var t=0; t<lines.length;t++){
+					var thisProjectData = lines[t];	
 				
-				
-			$(""+
-				"<div>"+  
-					"<h2>" + sp.projects +"</h2>"+
-					"<p>" + sp.project +"</p>"+
-					"<p>" + sp.pname +"</p>"+
-					"<p>" + sp.fname +"</p>"+
-					"<p>" + sp.lname +"</p>"+
-					"<p>" + sp.email +"</p>"+
-					"<p>" + sp.phone +"</p>"+
-					"<p>" + sp.emailOkay +"</p>"+
-					"<p>" + sp.cost +"</p>"+
-					"<p>" + sp.priority +"</p>"+
-					"<p>" + sp.startDate +"</p>"+
-					"<p>" + sp.jobNotes +"</p>"+
-				"</div>"
-			).appendTo("#dataHolder");
-			};
+				$(""+
+					"<div>"+  
+						"<h2>" + thisProjectData[0] +"</h2>"+
+						"<p>" + thisProjectData[1] +"</p>"+
+						"<p>" + thisProjectData[2] +"</p>"+
+						"<p>" + thisProjectData[3] +"</p>"+
+						"<p>" + thisProjectData[4] +"</p>"+
+						"<p>" + thisProjectData[5] +"</p>"+
+						"<p>" + thisProjectData[6] +"</p>"+
+						"<p>" + thisProjectData[7] +"</p>"+
+						"<p>" + thisProjectData[8] +"</p>"+
+						"<p>" + thisProjectData[9] +"</p>"+
+						"<p>" + thisProjectData[10] +"</p>"+
+						"<p>" + thisProjectData[11] +"</p>"+
+					"</div>"
+				).appendTo("#dataHolder");
+				}
+			}
 		});
+		return false;
 });
 });
-
 
 /*  temp
 
