0ad612b851da89853b376f21da0f53c403d2f37d
Matthew Richter <mrichter@fullsail.edu>
2012-07-19 21:13:25 -0400

    Trying to finish the contacts page but now I'm getting local server errors in couchDB 500 errors

--- asdcouch/asdcouch/app/_attachments/main.js
+++ asdcouch/asdcouch/app/_attachments/main.js
@@ -1,5 +1,5 @@
 // Matthew RIchter 
-// MIU - Full Sail University
+// ASD - Full Sail University
 // Project 3
 // July 19, 2012
 
@@ -379,6 +379,47 @@ $('#roofs').on('pageinit', function () {
 	    	}
 	    });
 });
+$('#contacts').on('pageinit', function () {   
+	$("#contactsList").empty();
+		$.ajax({
+	    	"url": 		"_view/roofs",
+	    	"dataType":	"json",
+	    	"success": function(data) {
+	    		console.log(data);
+	    		
+	    		$.each(data.rows, function(index, roof){
+//	    			var projectType = roof.value.projectType;
+	    			var projectName = roof.value.projectName;
+	    	    	var firstName = roof.value.firstName;
+	    	    	var lastName = roof.value.lastName;
+	    	    	var email = roof.value.email;
+	    	    	var phone = roof.value.phone;
+	    	    	var emailBest = roof.value.emailBest;
+	    	    	var cost = roof.value.cost;
+	    	    	var priority = roof.value.priority;
+	    	    	var startDate = roof.value.startDate;
+	    	    	var jobNotes = roof.value.jobNotes;
+
+	    	    		$("#contactsList").append(
+	    	    				$("<div>").attr("data-role", 
+	    	    						"collapsible").attr("data-collapsed", "true")
+	    	    						.append($("<h3>").text(projectName))
+	    	    						.append($("<p>").text(firstName))
+	    	    						.append($("<p>").text(lastName))
+	    	    						.append($("<p>").text(email))
+	    	    						.append($("<p>").text(phone))
+	    	    						.append($("<p>").text(emailBest))
+	    	    						.append($("<p>").text(cost))
+	    	    						.append($("<p>").text(priority))
+	    	    						.append($("<p>").text(startDate))
+	    	    						.append($("<p>").text(jobNotes))
+	    	    				);
+	    		});
+	    		$("#contactsList").listview("refresh");
+	    	}
+	    });
+});
+
 
 
 //});
